c4156340c5067e06b9d0f31114313f05c3f89daa
melgasteff <melgasteff@gmail.com>
2025-08-22 16:44:13 -0300

    fix: corregir imports

--- src/feature/inventario/color/infrastructure/typeorm/repository/color.typeorm.repository.ts
+++ src/feature/inventario/color/infrastructure/typeorm/repository/color.typeorm.repository.ts
@@ -1,12 +1,12 @@
 import { ColorRepository } from "@feature/inventario/color/domain/repository/color.repository";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ColorTypeORMModel } from "../model/color.typeorm.model";
-import { ILike, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { Color } from "@feature/inventario/color/domain/model/color.entity";
 import { NewColor } from "@feature/inventario/color/domain/model/new-color.entity";
+import { ColorTypeORMMapper } from "../mapper/color.typeorm.mapper";
 import { ColorMapper } from "../../mapper/color.mapper";
-import { ColorNotFoundException } from "@feature/inventario/color/application/exception/color-not-found.exception";
 
 @Injectable()
 export class ColorTypeORMRepository implements ColorRepository {
@@ -15,39 +15,28 @@ export class ColorTypeORMRepository implements ColorRepository {
         private readonly colorRepository: Repository<ColorTypeORMModel>
     ) { }
 
-    async create(color: NewColor): Promise<Color> {
-        const colorORM = await this.colorRepository.save(ColorMapper.toTypeORMModel(color))
-        return ColorMapper.toDomain(colorORM)
+    async create(newColor: NewColor): Promise<Color> {
+        const newColorORMModel = ColorTypeORMMapper.toORMModel(newColor)
+        const savedColorORMModel = await this.colorRepository.save(newColorORMModel)
+        return ColorTypeORMMapper.toDomain(savedColorORMModel)
     }
 
-    async getById(id: number): Promise<Color | null> {
-        const colorEntity = await this.colorRepository.findOne({ where: { id, eliminado: false } })
-        return colorEntity ? ColorMapper.toDomain(colorEntity) : null
+    async findById(id: number): Promise<Color | null> {
+        const colorORM = await this.colorRepository.findOne({where: { id, eliminado: false }})
+        if (colorORM == null) return null
+         return colorORM ? ColorMapper.toDomain(colorORM) : null
     }
 
-    async findByName(nombre: string): Promise<Color | null> {
-        const normalizedDescription = nombre.trim().toLowerCase();
-        const colorEntity = await this.colorRepository.findOne({
-            where: { descripcion: ILike(`%${normalizedDescription}%`), eliminado: false },
-        });
-        return colorEntity ? ColorMapper.toDomain(colorEntity) : null;
-    }
-
-    async update(id: number, descripcion: string): Promise<Color> {
-        await this.colorRepository.update(id, { descripcion });
-        const updatedColor = await this.colorRepository.findOneBy({ id });
-        if (!updatedColor) { throw new ColorNotFoundException(descripcion) }
-        return ColorMapper.toDomain(updatedColor);
+    async edit(color: Color): Promise<Color> {
+        const colorORM = ColorTypeORMMapper.toORMModel(color);
+        const saved = await this.colorRepository.save(colorORM);
+        return ColorTypeORMMapper.toDomain(saved);
     }
 
     async delete(id: number): Promise<void> {
-        const color = await this.colorRepository.findOne({ where: { id } });
-        if (!color) { throw new NotFoundException() }
+        const color = await this.colorRepository.findOneByOrFail({ id });
         color.eliminado = true;
         await this.colorRepository.save(color)
     }
 
-    count(): Promise<number> {
-        return this.colorRepository.count();
-    }
 }
