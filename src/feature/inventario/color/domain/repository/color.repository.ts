7d9866bbd0afadd1bfeb59a00e40a250e263fa47
melgasteff <melgasteff@gmail.com>
2025-08-22 16:38:20 -0300

    refactor: cambiar nombres

--- src/feature/inventario/color/domain/repository/color.repository.ts
+++ src/feature/inventario/color/domain/repository/color.repository.ts
@@ -3,9 +3,7 @@ import { NewColor } from "../model/new-color.entity";
 
 export abstract class ColorRepository {
         abstract create(color: NewColor): Promise<Color>;
-        abstract getById(id: number): Promise<Color | null>;
-        abstract findByName(nombre: string): Promise<Color |null>
-        abstract update(id: number, descripcion: string): Promise<Color>;
+        abstract edit(color: Color): Promise<Color>;
         abstract delete(id: number): Promise<void>;
-        abstract count(): Promise<number>;
+        abstract findById(id: number): Promise<Color | null>;
     }
