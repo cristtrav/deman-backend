f113c902ee75ddcdb0a9355255d370ee8c606902
melgasteff <melgasteff@gmail.com>
2025-08-22 16:43:15 -0300

    chore: eliminar caso de uso contar

--- src/feature/inventario/color/infrastructure/module/usecase-config.ts
+++ src/feature/inventario/color/infrastructure/module/usecase-config.ts
@@ -1,28 +1,19 @@
-import { ActualizarColorUseCase } from "@feature/inventario/color/application/usecase/actualizar.usecase";
-import { BuscarColorPorNombreUseCase } from "@feature/inventario/color/application/usecase/buscar-por-nombre.usecase";
-import { ContarColoresUseCase } from "@feature/inventario/color/application/usecase/contar.usecase";
 import { CrearColorUseCase } from "@feature/inventario/color/application/usecase/crear.usecase";
 import { EliminarColorUseCase } from "@feature/inventario/color/application/usecase/eliminar.usecase";
-import { ObtenerColorPorIdUseCase } from "@feature/inventario/color/application/usecase/obtener-por-id.usecase";
 import { ColorRepository } from "@feature/inventario/color/domain/repository/color.repository";
 import { Provider } from "@nestjs/common";
+import { EditarColorUseCase } from "../../application/usecase/editar.usecase";
+import { ConsultarColoresUseCase } from "../../application/usecase/consultar.usecase";
+import { ColorReadRepository } from "../../application/read-repository/color.read-repository";
+import { ConsultarColorPorIdUseCase } from "../../application/usecase/consultar-por-id.usecase";
 
 
 export default <Provider[]>[
     
-    {
-        provide: ContarColoresUseCase,
-        useFactory: (colorRepository: ColorRepository) => new ContarColoresUseCase(colorRepository),
-        inject: [ ColorRepository ]
-    },
+
     {
         provide: CrearColorUseCase,
         useFactory: (colorRepository: ColorRepository) => new CrearColorUseCase(colorRepository),
-        inject: [ ColorRepository ]
-    },
-    {
-        provide: ObtenerColorPorIdUseCase,
-        useFactory: (colorRepository: ColorRepository) => new ObtenerColorPorIdUseCase(colorRepository),
         inject: [ ColorRepository ]
     },
     {
@@ -31,13 +22,18 @@ export default <Provider[]>[
         inject: [ ColorRepository ]
     },
     {
-        provide: ActualizarColorUseCase,
-        useFactory: (colorRepository: ColorRepository) => new ActualizarColorUseCase(colorRepository),
+        provide: EditarColorUseCase,
+        useFactory: (colorRepository: ColorRepository) => new EditarColorUseCase(colorRepository),
         inject: [ ColorRepository ]
     },
     {
-        provide: BuscarColorPorNombreUseCase,
-        useFactory: (colorRepository: ColorRepository) => new BuscarColorPorNombreUseCase(colorRepository),
+        provide: ConsultarColoresUseCase,
+        useFactory: (colorReadRepository: ColorReadRepository) => new ConsultarColoresUseCase(colorReadRepository),
+        inject: [ ColorReadRepository ]
+    },
+    {
+        provide: ConsultarColorPorIdUseCase,
+        useFactory: (colorRepository: ColorRepository) => new ConsultarColorPorIdUseCase(colorRepository),
         inject: [ ColorRepository ]
     },
     
