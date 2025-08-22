c19814d744b01d8e67181a329562de38d87d4d56
melgasteff <melgasteff@gmail.com>
2025-08-22 16:43:43 -0300

    feat: actualizar informacion de las columnas

--- src/feature/inventario/color/infrastructure/typeorm/model/color.typeorm.model.ts
+++ src/feature/inventario/color/infrastructure/typeorm/model/color.typeorm.model.ts
@@ -1,13 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({schema: 'inventario', name:'color'})
+@Entity({ schema: 'inventario', name: 'color' })
 export class ColorTypeORMModel {
-    @PrimaryGeneratedColumn({name: 'id'})
-        id: number
-    
-        @Column({name: 'descripcion'})
-        descripcion: string
-    
-        @Column({name: 'eliminado'})
-        eliminado: boolean
+    @PrimaryGeneratedColumn('identity', { generatedIdentity: 'BY DEFAULT'})
+    id: number
+
+    @Column({ nullable: false, name: 'descripcion' })
+    descripcion: string
+
+    @Column({ nullable: false, name: 'eliminado', default: false })
+    eliminado: boolean
 }
