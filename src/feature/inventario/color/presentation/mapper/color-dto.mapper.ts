d20db9364f30551b8f713466f89b0cee8534bb17
melgasteff <melgasteff@gmail.com>
2025-08-22 16:44:45 -0300

    chore: eliminar campo "Eliminado"

--- src/feature/inventario/color/presentation/mapper/color-dto.mapper.ts
+++ src/feature/inventario/color/presentation/mapper/color-dto.mapper.ts
@@ -2,19 +2,17 @@ import { Color } from "@feature/inventario/color/domain/model/color.entity"
 import { ColorDTO } from "../dto/color.dto"
 
 export class ColorDTOMapper {
-    static toDTO (color: Color): ColorDTO{
-        return {
-            id: color.getId(),
-            descripcion: color.getDescripcion(),
-            eliminado : color.isEliminado()
+    static toDTO(color: Color): ColorDTO {
+            return {
+                id: color.id,
+                descripcion: color.descripcion
+            }
         }
-    }
 
     static toDomain(colorDTO: ColorDTO): Color{
         return new Color(
             colorDTO.id,
-            colorDTO.descripcion,
-            colorDTO.eliminado
+            colorDTO.descripcion            
         )
     }
 }
